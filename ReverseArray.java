fbf121b049787cd061c3d7d2af94033b6b1a3de7
Pranav <pranavpatil7743@gmail.com>
2024-01-18 13:05:28 +0530

    lots of changes after a lot of time

---
+++ ReverseArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {11, 1, 12, 13, 14, 15};
+        reverse(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    // USING TWO POINTERS o(n); else we can use two for loops o(n^2)
+    static void reverse(int[] arr) {
+        int start=0;
+        int end = arr.length-1;
+        while(start<end){
+            int swap =arr[start];
+            arr[start]=arr[end];
+            arr[end]= swap;
+            start++;
+            end--;
+        }
+    }
+}
